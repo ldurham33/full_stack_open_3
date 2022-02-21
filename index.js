74ea167091cfbdb93f1271e4ab1524c1963e3c2a
ldurham33 <99217609+ldurham33@users.noreply.github.com>
2022-02-21 01:56:21 -0500

    heroku

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const requestLogger = (request, response, next) => {
 app.use(express.json())
 app.use(requestLogger)
 app.use(cors())
+app.use(express.static('build'))
 morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 let persons = [
